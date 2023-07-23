c4e5b4bbb447f60acab7a2c3e5c8761bbf2409dc
123-Stephin <113153722+123-Stephin@users.noreply.github.com>
2023-07-23 12:01:41 +0530

    Firebase.js

--- src/component/Firebase.js
+++ src/component/Firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore/lite';
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyD1y-PtLbGeRi49PSsEm6mselFALBoqsIs",
-        authDomain: "project-bd1df.firebaseapp.com",
-        databaseURL: "https://project-bd1df-default-rtdb.firebaseio.com/",
-        projectId: "project-bd1df",
-        storageBucket: "project-bd1df.appspot.com",
-        messagingSenderId: "620350236071",
-        appId: "1:620350236071:web:9b6393b1b21b3f15dd0282"
-      };
+const firebaseConfig = {
+  apiKey: "AIzaSyDe1sqdD2SryUEcdayJC5pDNFIcl409Cj8",
+  authDomain: "cap-project-7ec00.firebaseapp.com",
+  databaseURL: "https://cap-project-7ec00-default-rtdb.firebaseio.com",
+  projectId: "cap-project-7ec00",
+  storageBucket: "cap-project-7ec00.appspot.com",
+  messagingSenderId: "1012177474754",
+  appId: "1:1012177474754:web:52481cedc26e41b953975b",
+  measurementId: "G-4QXC6QGF1Q"
+};
 
       const firebaseApp = initializeApp(firebaseConfig);
       const db = getFirestore(firebaseApp);
