28a5d06645d1d30f2c669956af556d3aa96798b0
123-Stephin <113153722+123-Stephin@users.noreply.github.com>
2023-07-23 12:00:14 +0530

    Add files via upload

---
+++ src/component/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div>
+            <img src={require("../public/Lovely-Professional-University-860x497.jpg")} alt='LPU' />
+    </div>
+  )
+}
+
+export default Test
